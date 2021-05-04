41c5d349acd4273d24acf1bd76e62e8ea4bdfcf9
Maxime Masson <maxime.masson@nurun.com>
2021-05-04 11:42:01 -0400

    script to dump and restore

---
+++ restore_db.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# This will restore the dumped db
+
+docker-compose exec -T mongodb sh -c 'mongorestore -d strapi -u '${1}' -p '${2}' --authenticationDatabase admin --archive' < ./app/mongo/initial_db.archive
