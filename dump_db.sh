5faf06871d9f9072800d7fdfc4844c2ca8a96dbd
mmasson <maximemasson30@gmail.com>
2021-05-04 22:14:59 -0400

    update restore and dump db scripts

---
+++ dump_db.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# This will dump the current database
+
+docker-compose exec -T mongodb sh -c 'mongodump -d strapi -u '${1}' -p '${2}' --authenticationDatabase admin --archive' > ./mongo/db.dump
--- restore_db.sh
+++ restore_db.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # This will restore the dumped db
 
-docker-compose exec -T mongodb sh -c 'mongorestore -d strapi -u '${1}' -p '${2}' --authenticationDatabase admin --archive' < ./app/mongo/initial_db.archive
+docker-compose exec -T mongodb sh -c 'mongorestore -d strapi -u '${1}' -p '${2}' --authenticationDatabase admin --archive' < ./mongo/db.dump
