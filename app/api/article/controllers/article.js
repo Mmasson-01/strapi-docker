183ad47144e9aa111e6ef7a2e5804c0552ec6054
mmasson <maximemasson30@gmail.com>
2021-05-08 11:37:26 -0400

    article find by slug

--- app/api/article/controllers/article.js
+++ app/api/article/controllers/article.js
@@ -1,8 +1,28 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+const { sanitizeEntity } = require("strapi-utils");
+module.exports = {
+  async findOne(ctx) {
+    const { slug } = ctx.params;
+
+    const entity = await strapi.services.article.findOne({ slug });
+    return sanitizeEntity(entity, { model: strapi.models.article });
+  },
+  //   async findOne(ctx) {
+  //     const { slug } = ctx.params;
+  //     try {
+  //       const article = await strapi.services.article.findOne({ slug });
+  //       return ctx.send(article);
+  //     } catch (error) {
+  //       ctx.status = 404;
+  //       return ctx.send({
+  //         success: false,
+  //       });
+  //     }
+  //   },
+};
