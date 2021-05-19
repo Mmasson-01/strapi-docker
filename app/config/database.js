5bd4c2182ed760934c26df89beb283323257ca35
mmasson <maximemasson30@gmail.com>
2021-05-19 15:28:48 -0400

    feat: set config for production server

--- app/config/database.js
+++ app/config/database.js
@@ -1,65 +1,15 @@
-module.exports = ({ env }) => {
-  if (env("NODE_ENV") === "development") {
-    return {
-      defaultConnection: "default",
-      connections: {
-        default: {
-          connector: "bookshelf",
-          settings: {
-            client: "sqlite",
-            filename: env("DATABASE_FILENAME", ".tmp/data.db"),
-          },
-          options: {
-            useNullAsDefault: true,
-          },
-        },
+module.exports = ({ env }) => ({
+  defaultConnection: "default",
+  connections: {
+    default: {
+      connector: "bookshelf",
+      settings: {
+        client: "sqlite",
+        filename: env("DATABASE_FILENAME", ".tmp/data.db"),
       },
-    };
-  } else {
-    if (env("DATABASE") === "mongodb") {
-      return {
-        defaultConnection: "default",
-        connections: {
-          default: {
-            connector: "mongoose",
-            settings: {
-              host: env("DATABASE_HOST", "mongodb"),
-              srv: env.bool("DATABASE_SRV", false),
-              port: env.int("DATABASE_PORT", 27017),
-              database: env("DATABASE_NAME", "strapi"),
-              username: env("DATABASE_USERNAME", "strapi"),
-              password: env("DATABASE_PASSWORD", "password"),
-            },
-            options: {
-              authenticationDatabase: env("AUTHENTICATION_DATABASE", null),
-              ssl: env.bool("DATABASE_SSL", false),
-            },
-          },
-        },
-      };
-    } else {
-      return {
-        defaultConnection: "default",
-        connections: {
-          default: {
-            connector: "bookshelf",
-            settings: {
-              client: "postgres",
-              host: env("DATABASE_HOST", "postgres"),
-              port: env.int("DATABASE_PORT", 27017),
-              database: env("DATABASE_NAME", "the_guide"),
-              username: env("DATABASE_USERNAME", "the_guide"),
-              password: env("DATABASE_PASSWORD", "password"),
-              ssl: {
-                rejectUnauthorized: false,
-              },
-            },
-            options: {
-              ssl: true,
-            },
-          },
-        },
-      };
-    }
-  }
-};
+      options: {
+        useNullAsDefault: true,
+      },
+    },
+  },
+});
